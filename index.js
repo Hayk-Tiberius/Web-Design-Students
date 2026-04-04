9f0f534a9fb6edfdae61dc9f984485d445c70615
Prus <87985449+Hayk-Tiberius@users.noreply.github.com>
2026-04-04 11:03:15 +0300

    Fixing deploy

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ const data = [
     lastName: "Diya",
     age: 14,
     bg: "#ff6b6b",
+    hobby: "Dance",
   },
   {
     image: "/img/img_background/Bero_Dmitriy_bg.png",
@@ -79,6 +80,7 @@ const data = [
     lastName: "Bero",
     age: 14,
     bg: "#1c7ed6",
+    hobby: "Football"
   },
   {
     image: "/img/img_background/Borisov_Maxim_bg.png",
@@ -86,6 +88,7 @@ const data = [
     lastName: "Maxim",
     age: 14,
     bg: "#cc5de8",
+    hobby: "Walk"
   },
   {
     image: "/img/img_background/Braslavskiy_Gleb_bg.png",
@@ -93,6 +96,7 @@ const data = [
     lastName: "Gleb",
     age: 14,
     bg: "#845ef7",
+    hobby: ""
   },
   {
     image: "/img/img_background/Kamiev_Muslim_bg.png",
@@ -100,99 +104,106 @@ const data = [
     lastName: "Kamiev",
     age: 14,
     bg: "#5c7cfa",
+    hobby: "Dzyudo"
   },
   {
     image: "/img/img_background/Klimenova_Varvara_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
+    name: "Klimenova",
+    lastName: "Varvara",
     age: 14,
     bg: "#339af0",
+    hobby: "Draw"
   },
   {
     image: "/img/img_background/Lomovskoy_Rodion_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
-    age: 14,
+    name: "Lomovskoy",
+    lastName: "Rodion",
+    age: 15,
     bg: "#22b8cf",
+    hobby: "Minecraft"
   },
   {
     image: "/img/img_background/Mayorov_Artem_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
+    name: "Mayorov",
+    lastName: "Artem",
     age: 14,
     bg: "#20c997",
+    hobby: "Counter-Strike2"
   },
   {
     image: "/img/img_background/Mishin_Ivan_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
-    age: 14,
+    name: "Mishin",
+    lastName: "Ivan",
+    age: 15,
     bg: "#51cf66",
+    hobby: "Sport"
   },
   {
     image: "/img/img_background/Muzyka_Anna_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
+    name: "Muzyka",
+    lastName: "Anna",
     age: 14,
     bg: "#94d82d",
+    hobby: "Draw"
   },
   {
     image: "/img/img_background/Nikishchenko_Daniil_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
+    name: "Nikishchenko",
+    lastName: "Daniil",
     age: 14,
     bg: "#fcc419",
+    hobby: "RailwayVibe"
   },
-  { image: "/img/img_background/Popov_Gleb_bg.png", name: "Muslim", lastName: "Kamiev", age: 14 },
+  { image: "/img/img_background/Popov_Gleb_bg.png", name: "Popov", lastName: "Gleb", age: 14, hobby: "VideoGames" },
+
   {
     image: "/img/img_background/Skvortsov_Elisey_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
+    name: "Skvortsov",
+    lastName: "Elisey",
     age: 14,
     bg: "#ff922b",
+    hobby: "Minecraft"
   },
   {
     image: "/img/img_background/Taychev_Roman.png",
-    name: "Muslim",
-    lastName: "Kamiev",
+    name: "Taychev",
+    lastName: "Roman",
     age: 14,
     bg: "#ff6b6b",
+    hobby: "Formula1"
   },
   {
     image: "/img/img_background/Filimonova_Tatyana_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
+    name: "Filimonova",
+    lastName: "Tatyana",
     age: 14,
     bg: "#e64980",
+    hobby: "Gorillaz"
   },
   {
     image: "/img/img_background/Shifrina_Sofya_bg.png",
-    name: "Muslim",
-    lastName: "Kamiev",
-    age: 14,
+    name: "Shifrina",
+    lastName: "Sofya",
+    age: 13,
     bg: "#7048e8",
+    hobby: "Roller skates"
   },
 ];
-// ---------- DATA (у тебя уже есть) ----------
-// layer_data
-// data
 
-// ---------- DOM ----------
 const aside = document.querySelector("aside");
 const layersContainer = document.querySelector(".layers");
 
-// ---------- СОЗДАЁМ БЛОК ИНФЫ ----------
 const info = document.createElement("div");
 info.className = "info";
 aside.append(info);
 
-// ---------- КНОПКА ↓ ----------
+
 const nextBtn = document.createElement("button");
 nextBtn.textContent = "↓";
 nextBtn.className = "next";
 aside.append(nextBtn);
 
-// ---------- СОЗДАЁМ LAYERS ----------
+
 layer_data.forEach((user, index) => {
   const img = document.createElement("img");
   img.src = user.image;
@@ -229,13 +240,14 @@ function updateAside(index) {
           <h2>Name: ${user.name}</h2>
           <h2>Last Name: ${user.lastName}</h2>
           <h2>Age: ${user.age}</h2>
+          <h2>Hobby: ${user.hobby}</h2>
         </div>
       </div>
     </div>
   `;
 }
 
-// ---------- СМЕНА СЛОЯ ----------
+
 function changeLayer(index) {
   layers[current].style.opacity = "0";
   layers[current].style.scale = "0.5";
@@ -248,7 +260,7 @@ function changeLayer(index) {
   updateAside(current);
 }
 
-// ---------- КНОПКА ↓ ----------
+
 nextBtn.addEventListener("click", () => {
   let next = current + 1;
   if (next >= layers.length) next = 0;
@@ -256,10 +268,10 @@ nextBtn.addEventListener("click", () => {
   changeLayer(next);
 });
 
-// ---------- СКРОЛЛ ----------
+
 window.addEventListener("wheel", (e) => {
   if (e.deltaY > 0) nextBtn.click();
 });
 
-// ---------- СТАРТ ----------
+
 updateAside(0);
